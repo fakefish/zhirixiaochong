172fd8c8eff5caf8bf493b7e7335aa381a204f44
王佳裕 <fakefish.wjy@gmail.com>
2015-02-06 12:00:54 +0800

    no message

--- build/list.js
+++ build/list.js
@@ -50,7 +50,7 @@ var ZhiChongItem = React.createClass({displayName: "ZhiChongItem",
 	render: function() {
 		var topic = this.props.topic;
 		return (
-			React.createElement("h3", null,  topic.title)
+			React.createElement("h3", null, React.createElement("a", {href:  "http://daily.zhihu.com/story/" + topic.id, target: "_blank"},  topic.title))
 		)
 	}
 })
@@ -58,6 +58,6 @@ var ZhiChongItem = React.createClass({displayName: "ZhiChongItem",
 React.render(
 	React.createElement(ZhiChong, {
 		url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Fnews-at.zhihu.com%2Fapi%2F4%2Fnews%2Flatest%22%20and%20itemPath%20%3D%20%22json.stories%22&format=json&callback=", 
-        pollInterval: 1000 *　60　*　5}),
+        pollInterval: 30000}),
 	document.body
 )
